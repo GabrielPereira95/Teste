518fbb4b3f0a67bfe38ff12c18ce9c058316d9f1
GabrielPereira95 <92458299+GabrielPereira95@users.noreply.github.com>
2023-02-02 17:44:58 -0300

    teste_form.cy.js

---
+++ teste_form.cy.js
@@ -0,0 +1,44 @@
+describe('Form Submission', () => {
+    it('teste funcional com dados validos', () => {
+      cy.visit('www.formulariocontato.com');
+  
+      cy.get('#Name').type('NomeTeste');
+  
+      cy.get('#Email').type('email@teste.com');
+
+      cy.get('#Company').type('Companhia teste');
+
+      cy.get('#Website').type('www.companytest.com');
+
+      cy.get('#Phone').type('51999999999');
+
+      cy.get('#Inquiry').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent malesuada dignissim ligula, eu ultrices ipsum eleifend a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.u');
+  
+      cy.get('#btnSubmit').click();
+  
+      cy.contains('Sucesso');
+    });
+  });
+
+
+  describe('Form Submission', () => {
+    it('teste com caracteres especiais no campo name', () => {
+      cy.visit('www.formulariocontato.com');
+  
+      cy.get('#Name').type('N0M& T&ST#@');
+  
+      cy.get('#Email').type('email@teste.com');
+
+      cy.get('#Company').type('Companhia teste');
+
+      cy.get('#Website').type('www.companytest.com');
+
+      cy.get('#Phone').type('51999999999');
+
+      cy.get('#Inquiry').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent malesuada dignissim ligula, eu ultrices ipsum eleifend a. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.u');
+  
+      cy.get('#btnSubmit').click();
+  
+      cy.contains('Campo name inválido.');
+    });
+  });
